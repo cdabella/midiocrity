7bc63e8d84a0085ac28288ec9ffc97aab25122c3
Will Gray <weeum.gray@gmail.com>
2021-04-25 19:28:30 -0700

    first stab at encoder, need to test

--- midiocrity/encoder.py
+++ midiocrity/encoder.py
@@ -1,35 +1,33 @@
 import torch
 
-class encoder():
-    def __init__(self, input_shape):
-        self.lstm_1 = torch.nn.LSTM(input_shape[0] / 2, batch_first=True)
-        self.lstm_2 = torch.nn.LSTM(128, batch_first=True)
-        self.lstm_3 = torch.nn.LSTM(32, batch_first=True)
-        self.fc_1 = torch.nn.Linear(1024, 128)
+
+class encoder:
+    '''
+    Encoder consists of 3 stacked Bidirectional LSTM Layers followed by 4 Fully-Connected Layers.
+    Input to LSTM is of size (batch size, seq_len, input_size) e.g. (128, 256, 1) for drums
+    Output from LSTM is of size (seq_len, batch, num_directions*hidden_size)
+    '''
+    def __init__(self, input_size, seq_len=256, hidden_size=32, num_layers=3):
+        self.lstm = torch.nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers,
+                                  bidirectional=True, batch_first=True)
+        self.fc_1 = torch.nn.Linear(seq_len * 2 * hidden_size, 128)
         self.fc_2 = torch.nn.Linear(128, 64)
         self.fc_3 = torch.nn.Linear(64, 32)
         self.fc_4 = torch.nn.Linear(32, 4)
 
-
-
-
+    '''
+    Build simple decoder with pytorch based on musae decoders.py
+    '''
     def forward(self, input):
-        # Build simple decoder with pytorch based on musae decoders.py
-        # Input is single channel X (dimensions time x pitch)
+        output, (h_n, c_n) = self.lstm(input)
 
-        # Reshape
-        output = torch.reshape(input, (input.shape[0] / 2, input.shape[1] * 2))
+        # Reshape, flattening each sample
+        output = torch.transpose(output, dim0=0, dim1=1)
+        output = torch.flatten(output, start_dim=1)
 
-        # 3 LSTM Layers
-        output = self.lstm_1(output)
-        output = self.lstm_2(output)
-        output = self.lstm_3(output)
-
-        # Reshape
-        output = torch.flatten(output)
-
-        # 3 Fully Connected Layers
+        # 4 Fully Connected Layers
         output = self.fc_1(output)
         output = self.fc_2(output)
         output = self.fc_3(output)
+        output = self.fc_4(output)
         return output
--- midiocrity/train.py
+++ midiocrity/train.py
@@ -1,11 +1,12 @@
 from midiocrity.data_unloader import unload_data
 from midiocrity import encoder, decoder, discriminator
+import torch
 
 
 class midiocrity():
     def __init__(self, **kwargs):
         print("Initializing encoder...")
-        self.encoder = encoder.build_encoder()
+        self.encoder = None
 
         print("Initializing decoder...")
         self.decoder = decoder.build_decoder()
@@ -13,13 +14,15 @@ class midiocrity():
         print("Initializing discriminator...")
         self.z_discriminator = discriminator.build_gaussian_discriminator()
 
-
     def train(self):
         batch = unload_data()
+        X = batch[0, :, :, :, 0]
+        X = torch.squeeze(X)
+        # single-track input X should now be of size (batch, time, pitch)
+        self.encoder = encoder.encoder(X.shape[2])
+        output = self.encoder.forward(X)
 
-
-
-
+        
 if __name__ == "__main__":
     midiocrity = midiocrity()
     midiocrity.train()
