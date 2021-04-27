c7cad84d503c3c09f8e6605edb2b1ac863e4bfd4
abella <abella@extrahop.com>
2021-04-27 19:16:47 -0400

    Decoder: Initial non-working

--- midiocrity/decoder.py
+++ midiocrity/decoder.py
@@ -1,3 +1,14 @@
+import torch
+import torch.nn as nn
+
+class Decoder(nn.Module):
+    def __init__(self, z_dim, n_tracks, hidden_layers, output_dim):
+        self.z_dim = z_dim
+        self.n_tracks = n_tracks
+        self.hidden_layers = hidden_layers
+        self.output_dim
+
+
 # flat version
 def build_decoder():
     # Build simple decoder with pytorch based on musae decoders.py
